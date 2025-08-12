bf52d0954b12a07da27bb9023bca32864e60608a
aqua <aqua.rock.slug@gmail.com>
2025-08-12 03:40:15 -0500

    player run and jump animations

--- player.gd
+++ player.gd
@@ -1,26 +1,34 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+		if velocity.y < 0:
+			_animated_sprite.play("jump")
+		else:
+			_animated_sprite.play("falling")
+
+	# jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Handle run
+	# run
 	# TODO replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		_animated_sprite.play("run")
+		_animated_sprite.flip_h = direction == -1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		_animated_sprite.play("idle")
 
 	move_and_slide()
