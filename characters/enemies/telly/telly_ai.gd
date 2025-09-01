e90c17ebaa0f53a2aa4c50366f1932b334c10259
aqua <aqua.rock.slug@gmail.com>
2025-09-01 07:44:00 -0500

    drill enemy

--- characters/enemies/telly/telly_ai.gd
+++ characters/enemies/telly/telly_ai.gd
@@ -1,7 +1,9 @@
 extends AI
 
 func _get_input_direction() -> Vector2:
-    if Global.player:
-        return owner.global_position.direction_to(Global.player.global_position)
-    else:
-        return Vector2.ZERO
+	if Global.player:
+		var target = Vector2(Global.player.global_position.x, owner.global_position.y)
+		owner.flip_direction = target.x > owner.global_position.x
+		return owner.global_position.direction_to(target)
+	else:
+		return Vector2.ZERO
