5612e6f74abda30c117176f6f0b5b64f1bb6df2f
aqua <aqua.rock.slug@gmail.com>
2025-10-02 00:20:09 -0500

    drill gravity fix

--- characters/enemies/drill/move.gd
+++ characters/enemies/drill/move.gd
@@ -12,7 +12,7 @@ func _enter() -> void:
 
 func _physics_process(delta: float) -> void:
 	_velocity = owner.get_facing_direction() * owner.speed * delta
-	_velocity.y += Constants.GRAVITY * gravity_scale
+	_velocity.y += Constants.GRAVITY * owner.gravity_scale
 	_velocity = owner.move_and_slide(_velocity)
 
 	for body in owner._player_collision_area.get_overlapping_bodies():
