51dabb73ab5fa8568f6bf65665e8aaf809b2a4cd
aqua <aqua.rock.slug@gmail.com>
2025-10-15 06:22:46 -0500

    nimbus demo animation

--- stages/deception/deception_stage.gd
+++ stages/deception/deception_stage.gd
@@ -1,22 +1,22 @@
 extends Stage
 
-# onready var _boss: Node2D
-# onready var _boss_door: Node2D
+onready var _boss: Node2D
+onready var _boss_door: Node2D
 
-# func _notification(what):
-# 	match what:
-# 		NOTIFICATION_INSTANCED:
-# 			_boss_door = $"BossDoors/BossDoor01"
-# 			_boss = $"Sections/Section06/RagWomen"
+func _notification(what):
+	match what:
+		NOTIFICATION_INSTANCED:
+			_boss_door = $"BossDoors/BossDoor01"
+			_boss = $"Sections/Section05/NimbusMan"
 
 func _connect_signals() -> void:
 	._connect_signals()
 
 	# Stage Boss Doors
-	# _try_connect(_boss_door, "closed", _boss, "on_boss_entered")
-	# _try_connect(_boss_door, "closed", player, "on_boss_entered")
+	_try_connect(_boss_door, "closed", _boss, "on_boss_entered")
+	_try_connect(_boss_door, "closed", player, "on_boss_entered")
 
-	# for boss_door in get_tree().get_nodes_in_group("BossDoors"):
-	# 	_try_connect(self, "restarted", boss_door, "on_restarted")
-	# 	for checkpoint in get_tree().get_nodes_in_group("Checkpoints"):
-	# 		_try_connect(checkpoint, "checkpoint_reached", boss_door, "on_checkpoint_reached")
+	for boss_door in get_tree().get_nodes_in_group("BossDoors"):
+		_try_connect(self, "restarted", boss_door, "on_restarted")
+		for checkpoint in get_tree().get_nodes_in_group("Checkpoints"):
+			_try_connect(checkpoint, "checkpoint_reached", boss_door, "on_checkpoint_reached")
