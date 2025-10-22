788839411ea420ab4a5701aa051d4cafe20ba558
aqua <aquarockslug@gmail.com>
2025-10-21 22:01:42 -0500

    kao na spray animation

--- characters/enemies/kao_na_wahna/enemy_kao_na_wahna.gd
+++ characters/enemies/kao_na_wahna/enemy_kao_na_wahna.gd
@@ -2,9 +2,14 @@ extends "res://characters/enemies/base/enemy_base.gd"
 
 # Similar to the Kao Na Gahna from mm8, but these totems spew water from the top instead to push the player
 
+onready var _enemy_animations: AnimationPlayer = $"EnemyAnimations"
+
 func _ready():
-	pass
-	# $"WaterSprite".play()
+	while true:
+		_enemy_animations.play("spray")
+		yield(_enemy_animations, "animation_finished")
+		_enemy_animations.play("idle")
+		yield(_enemy_animations, "animation_finished")
 
 func _process(delta):
 	pass
