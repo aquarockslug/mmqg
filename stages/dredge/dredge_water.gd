f8a7bfdddab6fddf840aca02432cec0a42066d4e
aqua <aquarockslug@gmail.com>
2025-10-25 06:02:22 -0500

    waterfalls

---
+++ stages/dredge/dredge_water.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+export(int) var water_power := 0.5
+
+onready var _jumping_player: State = $"../../../Player/StateMachine/Jump"
+
+func _process(delta):
+	for body in get_overlapping_bodies():
+		if (body.name == "Player"): 
+			_jumping_player.velocity.y += water_power * delta * 1000
