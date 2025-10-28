56438370c1c19fd52f750f9277c837f9c57c4f2a
aqua <aquarockslug@gmail.com>
2025-10-27 19:43:16 -0500

    namehage turn fix

--- characters/enemies/namehage_attacker/turn.gd
+++ characters/enemies/namehage_attacker/turn.gd
@@ -1,13 +1,10 @@
 extends State
 
 func _enter() -> void:
-	if owner.is_broken:
-		$"../../EnemyAnimations".play("turn_broken")
-	else:
-		$"../../EnemyAnimations".play("turn")
+	$"../../EnemyAnimations".play("turn_broken" if owner.is_broken else "turn")
 
 func _on_animation_finished(anim_name: String) -> void:
-	if anim_name == "turn" || anim_name == "turn_broken":
+	if anim_name == "turn" || anim_name == "turn_broken" || anim_name == "break":
 		$"../../Mask".position.x *= -1
 		$"../../Mask".flip_h = not $"../../Mask".flip_h
 		emit_signal("finished", "move")
