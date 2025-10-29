48af9da7af9b6855c9cf7ef962eb56caaa544a83
aqua <aquarockslug@gmail.com>
2025-10-28 22:55:06 -0500

    keep falling option on broken platforms

--- stages/assets/broken_platform.gd
+++ stages/assets/broken_platform.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-# change in y position per frame while falling
-export(float) var fall_speed = 4
+export(bool) var keep_falling = true
 
 # plays the shake animation
 onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
+# change in y position per frame while falling
+var fall_speed = 4
 var triggered = false
 var target_y = 0
 
@@ -19,12 +20,17 @@ func _process(delta):
 	if triggered && not _animation_player.is_playing() && position.y < target_y:
 		position.y += fall_speed
 	
+	# automatically trigger again when the target is reached
+	if keep_falling and triggered and position.y == target_y:
+		_animation_player.play("shake")
+	
 func _on_body_entered(body):
 	if body.name == "Player" || body.name == "Rock": _on_trigger()
 	
 # move the target_y after every shakes
 func _on_animation_finished(anim_name):
-	if anim_name == "shake": target_y += 16
+	if anim_name == "shake": 
+		target_y += 16
 		
 func _on_trigger():
 	_animation_player.play("shake")
