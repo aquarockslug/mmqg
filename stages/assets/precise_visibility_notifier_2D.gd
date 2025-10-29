bb491cc954e1113e2087e4711f85c4bae4e8eb98
aqua <aquarockslug@gmail.com>
2025-10-29 02:42:33 -0500

    export prep

--- stages/assets/precise_visibility_notifier_2D.gd
+++ stages/assets/precise_visibility_notifier_2D.gd
@@ -18,34 +18,34 @@ signal camera_entered()
 signal camera_exited()
 
 func _ready() -> void:
-    set_enabled(enabled)
-    _base_size = Vector2(Global.base_size.x, Global.base_size.y)
-    force_visibility_update()
-    _was_on_screen = is_on_screen()
+	set_enabled(enabled)
+	_base_size = Vector2(Global.base_size.x, Global.base_size.y)
+	force_visibility_update()
+	_was_on_screen = is_on_screen()
 
 func _physics_process(delta: float) -> void:
-    force_visibility_update()
-    _is_on_screen = is_on_screen()
+	force_visibility_update()
+	_is_on_screen = is_on_screen()
 
-    if not _was_on_screen and _is_on_screen:
-        emit_signal("camera_entered")
-    elif _was_on_screen and not _is_on_screen:
-        emit_signal("camera_exited")
-    
-    _was_on_screen = _is_on_screen
+	if not _was_on_screen and _is_on_screen:
+		emit_signal("camera_entered")
+	elif _was_on_screen and not _is_on_screen:
+		emit_signal("camera_exited")
+	
+	_was_on_screen = _is_on_screen
 
 func is_on_screen() -> bool:
-    _sign = 1 if _was_on_screen else -1
-    _viewport_rect = _viewport_rect.grow(_sign * margin)
-    return _viewport_rect.intersects(_rect)
+	_sign = 1 if _was_on_screen else -1
+	_viewport_rect = _viewport_rect.grow(_sign * margin)
+	return _viewport_rect.intersects(_rect)
 
 func force_visibility_update() -> void:
-    _viewport_rect = Rect2(-_viewport.get_canvas_transform().origin, _base_size)
-    _rect = Rect2(global_position + rect.position, rect.size)
+	_viewport_rect = Rect2(-_viewport.get_canvas_transform().origin, _base_size)
+	_rect = Rect2(global_position + rect.position, rect.size)
 
 func set_enabled(value: bool) -> void:
-    set_physics_process(value)
-    enabled = value
+	set_physics_process(value)
+	enabled = value
 
 func is_enabled() -> bool:
-    return is_physics_processing()
+	return is_physics_processing()
