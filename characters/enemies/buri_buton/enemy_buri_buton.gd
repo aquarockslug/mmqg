81a0a114d8c76176c1f70707b1dcbaaa75ccf349
aqua <aquarockslug@gmail.com>
2025-10-31 03:47:28 -0500

    buri fixes

--- characters/enemies/buri_buton/enemy_buri_buton.gd
+++ characters/enemies/buri_buton/enemy_buri_buton.gd
@@ -5,19 +5,30 @@ onready var _ray: RayCast2D = $"Collider/LineOfSite"
 
 export(int) var move_speed := 1000
 export(int) var damage := 2
-export(int) var gravity_scale := 4
+export(int) var gravity_scale := 5
 
-var speed := 1000
+var speed = 1000 # the current speed
+var stuck_time = 0 # the number of _physics_processes spent being stuck
+var turn_threshold = 10 # how long to be stuck before turning
+
 func _physics_process(delta: float) -> void:
 	_velocity = get_facing_direction() * speed * delta
 	_velocity.y += Constants.GRAVITY * gravity_scale
 	_velocity = move_and_slide(_velocity)
 	
 	# dont move forward while falling
-	if falling(): speed = 0 
-	
-	# turn around if the path is being blocked
-	if stuck(): turn_around()
+	if falling(): 
+		speed = 0
+		_velocity.x = 0 
+		
+func _process(delta: float) -> void:
+	# count how many _physics_processes have been made while stuck
+	if stuck(): stuck_time += 1
+		
+	# turn around if stuck for too long
+	if stuck_time >= turn_threshold:
+		turn_around()
+		stuck_time = 0
 
 # true when in the air with velocity on the y axis
 func falling() -> bool: return _velocity.y != 0 && not is_on_floor()
@@ -31,4 +42,3 @@ func turn_around() -> void:
 
 func can_see_player() -> bool:
 	return _ray.is_colliding()
-	
