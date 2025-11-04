ee09409d11e6d782e0e9eafb7c8c85b2496cc27c
aqua <aquarockslug@gmail.com>
2025-11-03 20:08:18 -0600

    bricky shake

--- characters/enemies/bricky/enemy_bricky.gd
+++ characters/enemies/bricky/enemy_bricky.gd
@@ -1,7 +1,12 @@
 extends "res://characters/enemies/base/enemy_base.gd"
 
 func _ready():
-	pass
+	$HitBox.connect("body_entered", self, "_on_body_entered")
 
-#func _process(delta):
-#	pass
+func _on_body_entered(body):
+	if $StateMachine.current_state.name == "Triple":
+		$EnemyAnimations.play("triple_shake")
+	if $StateMachine.current_state.name == "Double":
+		$EnemyAnimations.play("double_shake")
+	if $StateMachine.current_state.name == "Single":
+		$EnemyAnimations.play("single_shake")
