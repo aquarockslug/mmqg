b1cc1168d026c771c69bb4d6c95c5e192df151f6
aqua <aquarockslug@gmail.com>
2025-11-03 18:19:03 -0600

    item color fix

--- items/weapon_energy/weapon_energy.gd
+++ items/weapon_energy/weapon_energy.gd
@@ -3,7 +3,7 @@ extends "res://items/pick_up_base/pick_up_base.gd"
 export(int) var energy_charged := 10
 
 func _ready() -> void:
-    $"Sprite/AnimationPlayer".play("Blink")
+	$"Sprite/AnimationPlayer".play("Blink")
 
 func _on_picked_up_effect(body: Player) -> void:
-    body.charge_weapon(energy_charged)
+	body.charge_weapon(energy_charged)
