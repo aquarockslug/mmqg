567283e231e50f466b4579c9056a96e505cfc8c2
aqua <aquarockslug@gmail.com>
2025-11-04 23:00:25 -0600

    buri tweaks

--- characters/enemies/buri_buton/enemy_buri_buton.gd
+++ characters/enemies/buri_buton/enemy_buri_buton.gd
@@ -5,36 +5,37 @@ onready var _ray: RayCast2D = $"Collider/LineOfSite"
 
 export(int) var move_speed := 1000
 export(int) var damage := 2
-export(int) var gravity_scale := 5
+export(int) var gravity_scale := 10
 
 var speed = 1000 # the current speed
 var stuck_time = 0 # the number of _physics_processes spent being stuck
 var turn_threshold = 30 # how long to be stuck before turning
 
 func _physics_process(delta: float) -> void:
-	_velocity = get_facing_direction() * speed * delta
-	_velocity.y += Constants.GRAVITY * gravity_scale
-	_velocity = move_and_slide(_velocity)
-	
 	# dont move forward while falling
 	if falling(): 
-		speed = 0
 		_velocity.x = 0 
+	else:
+		_velocity = get_facing_direction() * speed * delta 
+	
+	_velocity.y += Constants.GRAVITY * gravity_scale * delta
+	_velocity = move_and_slide(_velocity)
 		
 func _process(delta: float) -> void:
 	# count how many _processes have been made while stuck
 	if stuck(): stuck_time += 1
-		
+	
 	# turn around if stuck for too long
 	if stuck_time >= turn_threshold:
 		turn_around()
+		emit_signal("change_state", "move")
 		stuck_time = 0
 
 # true when in the air with velocity on the y axis
-func falling() -> bool: return _velocity.y != 0 && not is_on_floor()
+func falling() -> bool: return _velocity.y != 0 and not is_on_floor()
 
 # true when trying to move but a collision is preventing it
-func stuck() -> bool: return _velocity.x == 0 && speed != 0
+func stuck() -> bool: return _velocity.x == 0 and not falling()
 
 func turn_around() -> void:
 	toggle_flip_h()
--- characters/enemies/buri_buton/rush.gd
+++ characters/enemies/buri_buton/rush.gd
@@ -2,10 +2,10 @@ extends State
 
 func _enter() -> void:
 	$"../../EnemyAnimations".play("rush")
-	owner.speed = owner.move_speed * 2
+	owner.speed = owner.move_speed * 2.5
 
-func _process(delta: float) -> void:
-	if not owner.can_see_player(): emit_signal("finished", "move")
+#func _process(delta: float) -> void:
+#	if owner.stuck(): emit_signal("finished", "move")
 
 func _physics_process(delta: float) -> void:
 	for body in owner._player_collision_area.get_overlapping_bodies():
