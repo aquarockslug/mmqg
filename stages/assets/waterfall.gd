85337ad2fda3e4e65d0fb0e8c10465c41bbea9df
aqua <aquarockslug@gmail.com>
2025-11-06 17:50:41 -0600

    splash fixes

--- stages/assets/waterfall.gd
+++ stages/assets/waterfall.gd
@@ -8,7 +8,7 @@ onready var _splash_sprite: Sprite = $SplashSprite # TODO use an AnimatedSprite
 
 var is_splashing = false
 var player = null
-var splash_position = Vector2(0, -15)
+var splash_position = Vector2(0, -13)
 var animation_frame_duration = 0.1
 
 func _ready():
@@ -32,13 +32,12 @@ func _on_body_entered(body):
 	if not body is Player: return
 	else: player = body as Player
 
-	_sfx.play()
-
 	is_splashing = true
 	_splash_sprite.visible = true
 
 	# animate splash sprite while splashing
 	while is_splashing:
+		_sfx.play()
 		_splash_sprite.frame = 0 if _splash_sprite.frame else 1
 		yield(get_tree().create_timer(animation_frame_duration), "timeout")
 
