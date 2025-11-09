209f41042c5b3216fa8fbb0f873a6d0ea169cd67
aqua <aquarockslug@gmail.com>
2025-11-09 08:06:04 -0600

    dige hitbox

--- characters/enemies/dig_e/death.gd
+++ characters/enemies/dig_e/death.gd
@@ -1,8 +1,10 @@
 extends State
 
+export(int) var explosion_scale := 4
 onready var _animations: AnimationPlayer = $"../../EnemyAnimations"
 
 func _enter() -> void:
+	owner._player_collision_area.scale = Vector2(explosion_scale, explosion_scale)
 	$"../../SFX/Explode".play()
 	_animations.play("explode")
 	_animations.connect("animation_finished", self, "_on_animation_finished")
--- characters/enemies/dig_e/enemy_dige.gd
+++ characters/enemies/dig_e/enemy_dige.gd
@@ -1,7 +1,5 @@
 extends "res://characters/enemies/base/enemy_base.gd"
 
-export(int) var explosion_scale := 4
-
 onready var _timer_fuse: Timer = $TimerBombFuse
 
 export(int) var damage := 2
@@ -18,6 +16,5 @@ func _ready() -> void:
 func _on_explode():
 	_timer_fuse.stop()
 	exploded = true
-	_player_collision_area.scale = Vector2(explosion_scale, explosion_scale)
 	set_physics_process(false) # stop the physics process loop
 	emit_signal("change_state", "death")
