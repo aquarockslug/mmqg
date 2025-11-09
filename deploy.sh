df26ea4f8efc38e46faf3879a396eea869ab62b3
aqua <aquarockslug@gmail.com>
2025-11-09 05:43:22 -0600

    deploy script more platforms

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
+
+# HTML5
 rm ./builds/html5/build.zip
 7z a ./builds/html5/build.zip ./builds/html5/*
 butler push ./builds/html5/build.zip aquarock/mmqg:html5
+
+# WINDOWS
+rm ./builds/windows/build.zip
+7z a ./builds/windows/build.zip ./builds/windows/*
+butler push ./builds/windows/build.zip aquarock/mmqg:windows
+
+# LINUX
+rm ./builds/linux/build.zip
+7z a ./builds/linux/build.zip ./builds/linux/*
+butler push ./builds/linux/build.zip aquarock/mmqg:linux
