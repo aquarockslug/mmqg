dabba6fbbd16ce2834e810291920890829455574
Aqualava <aqua.rock.slug@gmail.com>
2025-11-10 05:18:51 -0600

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if test -x /bin/timg
 end
 
 echo HTML5
-# godot3 --no-window --export HTML5 builds/windows/mmqg.exe
+# godot3 --no-window --export HTML5 builds/html5/mmqg.exe
 rm ./builds/html5/build.zip
 7z a ./builds/html5/build.zip ./builds/html5/*
 butler push ./builds/html5/build.zip aquarock/mmqg:html5
